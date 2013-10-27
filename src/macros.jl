1d1fa88cb6c8ad6604a0c41b724ecf85919344da
Jake Bolewski <jakebolewski@gmail.com>
2013-10-27 04:30:49 -0400

    split out toplevel macros

---
+++ src/macros.jl
@@ -0,0 +1,15 @@
+macro ocl_call(func, arg_types, args...)
+    quote
+        _err = ccall(($func, libopencl), CL_int, $arg_types, $(args...))
+        if _err != CL_SUCCESS
+            error("CL ERROR: $func")
+        end
+    end
+end
+
+macro ocl_func(func, arg_types)
+    local args_in = Symbol[symbol(string('a', i)) for i in 1:length(arg_types.args)]
+    quote
+        $(esc(func))($(args_in...)) = @ocl_call($(string(func)), $arg_types, $(args_in...))
+    end
+end
