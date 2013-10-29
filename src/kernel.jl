35afaad8d935ae2ff2257dd1923cdc180dd5977d
Jake Bolewski <jakebolewski@gmail.com>
2013-10-29 16:58:33 -0400

    stub kernel/program implementation

---
+++ src/kernel.jl
@@ -0,0 +1,24 @@
+type Kernel
+end
+
+type KernelArg
+end
+
+type LocalMemory
+    size::Csize_t
+end
+#TODO: local_memory
+
+#TODO: get_info(k::Kernel, param)
+#TODO: get_work_group_info(k::Kernel, param, d::Device)
+#TODO: get_arg_info(k::Kernel, idx, param)
+#TODO: set_arg(k::Kernel, idx, arg)
+#TODO: set_args(k::Kernel, args...)
+#TODO: call(queue, global_size, local_size, *args, global_offset=None, wait_for=None)
+#TODO: ?macro capture_call
+
+#TODO: enqueue_nd_range_kernel(queue, kernel, global_work_size, local_work_size,
+#                              global_work_offset=None, wait_for=None)
+
+#TODO: enqueue_task(q::Queue, k::Kernel; wait_for=None)
+#TODO: enqueue_async_kernel()
--- src/program.jl
+++ src/program.jl
@@ -1,6 +1,35 @@
 type Program
+    ptr::CL_program
 end
 
+@ocl_func(clReleaseProgram, (CL_program,))
+
+function release!(p::Program)
+    if p.ptr != C_NULL
+        clReleaseProgram(p.ptr)
+        p.ptr = C_NULL
+    end
+end
+
+# low level api 
+function clBuildProgram()
+end
+
+function clGetProgramBuildInfo()
+end
+
+function clCreateProgramWithBinary()
+end
+
+# high level api
+function build_program(p::Program, d::Device)
+end
+
+function create_program_with_binary(ctx::Context, d::Device, binary::String)
+end
+
+#TODO: create_program_from_source()
+#TODO: create_program_from_binary()
 
 #TODO: get_info
 #TODO: get_build_info()
