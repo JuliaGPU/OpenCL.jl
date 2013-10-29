9fc0bbe9d51345b4fd64d02946109dc79349cdb9
Jake Bolewski <jakebolewski@gmail.com>
2013-10-29 16:20:57 -0400

    add program.jl stub

---
+++ src/program.jl
@@ -0,0 +1,14 @@
+type Program
+end
+
+
+#TODO: get_info
+#TODO: get_build_info()
+#TODO: build(program; options=[], devices=None)
+#TODO: compile(program, options=[], devices=nothing, headers=[])
+#TODO: all_kernels(program)
+
+#OpenCL 1.2
+#TODO: create_program_with_built_in_kernels(ctx, devices, kernel_names)
+#TODO: link_program(ctx, programs; options=[], devices=None)
+#TODO: unload_platform_compiler(platform)
