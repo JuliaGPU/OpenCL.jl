a44e2117646349783df04d60001602b471e37e59
Jake Bolewski <jakebolewski@gmail.com>
2013-10-29 17:34:02 -0400

    split function generation and error checking macros

--- src/macros.jl
+++ src/macros.jl
@@ -7,9 +7,24 @@ macro ocl_call(func, arg_types, args...)
     end
 end
 
+macro ocl_call2(func, ret_type, arg_types, args...)
+    quote
+        ccall(($func, libopencl), $ret_type, $arg_types, $(args...))
+    end
+end
+
 macro ocl_func(func, arg_types)
     local args_in = Symbol[symbol(string('a', i)) for i in 1:length(arg_types.args)]
     quote
         $(esc(func))($(args_in...)) = @ocl_call($(string(func)), $arg_types, $(args_in...))
+    end
+end
+
+macro ocl_check(clfunc)
+    quote
+        _err = $clfunc
+        if _err != CL_SUCCESS
+            error("CL_ERROR: $func")
+        end
     end
 end
