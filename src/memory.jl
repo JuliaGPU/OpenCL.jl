00d3af67c4b36e962c164727a0b5dfca21b164ca
Jake Bolewski <jakebolewski@gmail.com>
2013-11-11 18:09:09 -0500

    info() for mem objects

--- src/memory.jl
+++ src/memory.jl
@@ -1,6 +1,6 @@
 abstract CLMemObject
 
-#TODO: this should be implemented by all subtypes
+#This should be implemented by all subtypes
 #type MemObject <: CLMemory
 #    valid :: Bool
 #    ptr   :: CL_mem
@@ -25,13 +25,14 @@ Base.sizeof(mem::CL_mem) = begin
     return val[1]
 end
 
-
 function release!(mem::CLMemObject)
     if !mem.valid
         error("OpenCL.MemObject relase! error: trying to double unref mem object")
     end
-    @check_release api.clReleaseMemObject(mem.id)
-    mem.id = C_NULL
+    if mem.id != C_NULL
+        @check_release api.clReleaseMemObject(mem.id)
+        mem.id = C_NULL
+    end
     mem.valid = false
 end
 
@@ -42,27 +43,58 @@ context(mem::CLMemObject) = begin
     return Context(param[1])
 end
 
-macro memobj_property(func, cl_device_info, return_type)
-    @eval begin
-        function $func(mem::CLMemObject)
-            result = Array($return_type, 1)
-            @check api.clGetMemObjectInfo(mem.id, $cl_device_info,
-                                          sizeof($return_type), result, C_NULL)
-            return result[1]
+
+let mem_type(m::CLMemObject) = begin
+        result = Array(CL_mem_object_type, 1)
+        @check api.clGetMemObjectInfo(m.id, CL_MEM_TYPE, 
+                        sizeof(CL_mem_object_type), result, C_NULL)
+        return result[1]
+    end
+
+    size(m::CLMemObject) = begin
+        result = Array(Csize_t, 1)
+        @check api.clGetMemObjectInfo(m.id, CL_MEM_SIZE,
+                        sizeof(Csize_t), result, C_NULL)
+        return result[1]
+    end
+
+    reference_count(m::CLMemObject) = begin
+        result = Array(CL_uint, 1)
+        @check api.clGetMemObjectInfo(m.id, CL_MEM_REFERENCE_COUNT,
+                        sizeof(CL_uint), result, C_NULL)
+        return result[1]
+    end
+
+    map_count(m::CLMemObject) = begin
+        result = Array(CL_uint, 1)
+        @check api.clGetMemObjectInfo(m.id, CL_MEM_MAP_COUNT,
+                        sizeof(CL_uint), result, C_NULL)
+        return result[1]
+    end
+
+    info_map = (Symbol => Function)[
+        :mem_type => mem_type,
+        :size => size,
+        :reference_count => reference_count,
+        :map_count => map_count
+    ]
+
+    function info(mem::CLMemObject, minfo::Symbol)
+        try
+            func = info_map[minfo]
+            func(mem)
+        catch err
+            if isa(err, KeyError)
+                error("OpenCL.MemObject has no info for: $minfo")
+            else
+                throw(err)
+            end
         end
     end
 end
 
-@memobj_property(mem_type,        CL_MEM_TYPE,            CL_mem_object_type)
-@memobj_property(size,            CL_MEM_SIZE,            Csize_t)
-@memobj_property(reference_count, CL_MEM_REFERENCE_COUNT, CL_uint)
-@memobj_property(map_count,       CL_MEM_MAP_COUNT,       CL_uint)
 
-#TODO: base...
-
-#TODO: function get_info()
 #TODO: function hostbuf()
-#TODO: release()
 #TODO: host_array()
 #TODO: pointer
 
