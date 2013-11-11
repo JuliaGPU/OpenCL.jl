7a170b10e11cf56c1f6092aecb05fe8e3abc692d
Jake Bolewski <jakebolewski@gmail.com>
2013-11-11 12:31:30 -0500

    add additional tests for queue constructors, with carve out for POCL errors

--- test/component_tests.jl
+++ test/component_tests.jl
@@ -216,13 +216,29 @@ facts("OpenCL.Context") do
 end
 
 facts("OpenCL.CmdQueue") do 
-    context("OpenCL.CmdQueue device constructor") do
+    context("OpenCL.CmdQueue constructor") do
         @fact @throws_pred(cl.CmdQueue(nothing, nothing)) => (true, "error")
         for platform in cl.platforms()
+            if platform[:name] == "Portable Computing Language"
+                @fact "Portable Computing Language fails on :out_of_order" => true
+                continue
+            end
             for device in cl.devices(platform)
                 ctx = cl.Context(device)
                 @fact @throws_pred(cl.CmdQueue(ctx)) => (false, "no error")
                 @fact @throws_pred(cl.CmdQueue(ctx, device)) => (false, "no error")
+                for flag in [:profile, :out_of_order, (:profile, :out_of_order)]
+                    @fact @throws_pred(cl.CmdQueue(ctx, flag)) => (false, "no error")
+                    @fact @throws_pred(cl.CmdQueue(ctx, device, flag)) => (false, "no error")
+                end
+                @fact @throws_pred(cl.CmdQueue(ctx, :unrecognized_flag)) => (true, "error")
+                @fact @throws_pred(cl.CmdQueue(ctx, device, :unrecognized_flag)) => (true, "error")
+                for flag in [:profile, :out_of_order]
+                    @fact @throws_pred(cl.CmdQueue(ctx, (flag, :unrecognized_flag))) => (true, "error")
+                    @fact @throws_pred(cl.CmdQueue(ctx, device, (:unrecognized_flag, flag))) => (true, "error")
+                    @fact @throws_pred(cl.CmdQueue(ctx, (flag, flag))) => (true, "error")
+                    @fact @throws_pred(cl.CmdQueue(ctx, device, (flag, flag))) => (true, "error")
+                end
             end
         end
     end
