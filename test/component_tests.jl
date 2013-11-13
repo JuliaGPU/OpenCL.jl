eee59b502ee5e811a6e39fd32da28dfacd264d10
Jake Bolewski <jakebolewski@gmail.com>
2013-11-13 18:54:56 -0500

    increase coverage of buffer tests across devices

--- test/component_tests.jl
+++ test/component_tests.jl
@@ -492,43 +492,85 @@ facts("OpenCL.Buffer") do
      
          end
      end
-
+        ctx = cl.create_some_context()
+        queue = cl.CmdQueue(ctx)
+        testarray = zeros(Float32, 1000)
+        buf = cl.Buffer(Float32, ctx, (:rw, :copy), hostbuf=testarray)
+        return (queue, buf, testarray)
+ 
      context("OpenCL.Buffer fill") do
-        queue, buf, testarray = create_test_buffer()
-        
-        @fact buf.size == sizeof(testarray) => true
-        cl.fill!(queue, buf, float32(1.0))
-        readback = cl.read(queue, buf)
-        @fact all(x -> x == 1.0, readback) => true
-        @fact all(x -> x == 0.0, testarray) => true
-        @fact buf.valid => true
+        for device in cl.devices()
+             ctx = cl.Context(device)
+             queue = cl.CmdQueue(ctx)
+             testarray = zeros(Float32, 1000)
+             buf = cl.Buffer(Float32, ctx, (:rw, :copy), hostbuf=testarray)
+             @fact buf.size == sizeof(testarray) => true
+             try 
+                 cl.fill!(queue, buf, float32(1.0))
+                 readback = cl.read(queue, buf)
+                 @fact all(x -> x == 1.0, readback) => true
+                 @fact all(x -> x == 0.0, testarray) => true
+                 @fact buf.valid => true
+             catch err
+                #TODO: check to see if device platform implements 1.2 => error else noerror
+                warn("fill is a OpenCL v1.2 command")
+            end
+        end
     end
 
     context("OpenCL.Buffer write!") do
-        queue, buf, testarray = create_test_buffer()
+        for device in cl.devices()
+            ctx = cl.Context(device)
+            queue = cl.CmdQueue(ctx)
+            testarray = zeros(Float32, 1000)
+            buf = cl.Buffer(Float32, ctx, (:rw, :copy), hostbuf=testarray)
         
-        @fact buf.size == sizeof(testarray) => true
-        cl.write!(queue, buf, ones(Float32, length(testarray)))
-        readback = cl.read(queue, buf)
-        @fact all(x -> x == 1.0, readback) => true
-        @fact buf.valid => true
+            @fact buf.size == sizeof(testarray) => true
+            cl.write!(queue, buf, ones(Float32, length(testarray)))
+            readback = cl.read(queue, buf)
+            @fact all(x -> x == 1.0, readback) => true
+            @fact buf.valid => true
+        end
     end
 
     context("OpenCL.Buffer empty") do
-        ctx = cl.create_some_context()
-        testarray = zeros(Float32, 1000)
-        buf = cl.Buffer(Float32, ctx, (:rw, :copy), hostbuf=testarray)
+        for device in cl.devices()
+            ctx = cl.Context(device)
+            queue = cl.CmdQueue(ctx)
+            testarray = zeros(Float32, 1000)
+            buf = cl.Buffer(Float32, ctx, (:rw, :copy), hostbuf=testarray)
+           
+            @fact sizeof(cl.empty_like(ctx, buf)) => sizeof(testarray)
 
-        @fact @throws_pred(cl.empty(Float32, ctx, -1)) => (true, "error") 
-        empty_buf = cl.empty(Float32, ctx, 1000)
-        @fact empty_buf.size => sizeof(testarray)
-        @fact empty_buf.size => buf.size
-       
-        dims = (100, 100)
-        testarray = zeros(Float32, dims)
-        empty_buf = cl.empty(Float32, ctx, dims)
-        @fact empty_buf.size => sizeof(testarray)
-        @fact empty_buf.valid => true
+            @fact @throws_pred(cl.empty(Float32, ctx, -1)) => (true, "error") 
+            empty_buf = cl.empty(Float32, ctx, 1000)
+            @fact empty_buf.size => sizeof(testarray)
+            @fact empty_buf.size => buf.size
+           
+            dims = (100, 100)
+            testarray = zeros(Float32, dims)
+            empty_buf = cl.empty(Float32, ctx, dims)
+            @fact empty_buf.size => sizeof(testarray)
+            @fact empty_buf.valid => true
+        end
+    end
+
+    context("OpenCL.Buffer copy!") do
+        for device in cl.devices()
+            ctx = cl.Context(device)
+            queue = cl.CmdQueue(ctx)
+            test_array = fill(float32(2.0), 1000)
+            a_buf = cl.Buffer(Float32, ctx, sizeof(test_array))
+            b_buf = cl.Buffer(Float32, ctx, sizeof(test_array))
+            c_arr = Array(Float32, size(test_array))
+            # host to device buffer
+            cl.copy!(queue, a_buf, test_array)
+            # device buffer to device buffer
+            cl.copy!(queue, b_buf, a_buf)
+            # device buffer to host
+            cl.copy!(queue, c_arr, b_buf)
+            @fact all(x -> isapprox(x, 2.0), c_arr) => true
+        end
     end
 end
 
