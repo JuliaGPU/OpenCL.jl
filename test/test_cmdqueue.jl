5c413160b5c223112a37e5ac77b631e79b73f593
Jake Bolewski <jakebolewski@gmail.com>
2013-11-13 21:59:22 -0500

    remove gc calls in cmdqueue test

--- test/test_cmdqueue.jl
+++ test/test_cmdqueue.jl
@@ -7,9 +7,7 @@ const cl = OpenCL
 macro throws_pred(ex) FactCheck.throws_pred(ex) end 
 
 facts("OpenCL.CmdQueue") do 
-    # TODO:
-    # travis builds fail without gc's, throwing host out of memory errors
-    # see if handle to CmdQueue is not being released properly
+    
     context("OpenCL.CmdQueue constructor") do
         @fact @throws_pred(cl.CmdQueue(nothing, nothing)) => (true, "error")
         for platform in cl.platforms()
@@ -21,16 +19,13 @@ facts("OpenCL.CmdQueue") do
                 ctx = cl.Context(device)
                 @fact @throws_pred(cl.CmdQueue(ctx)) => (false, "no error")
                 @fact @throws_pred(cl.CmdQueue(ctx, device)) => (false, "no error")
-                Base.gc()
                 for flag in [:profile, :out_of_order, (:profile, :out_of_order)]
                     @fact @throws_pred(cl.CmdQueue(ctx, flag)) => (false, "no error")
                     @fact @throws_pred(cl.CmdQueue(ctx, device, flag)) => (false, "no error")
                 end
-                Base.gc()
                 @fact @throws_pred(cl.CmdQueue(ctx, :unrecognized_flag)) => (true, "error")
                 @fact @throws_pred(cl.CmdQueue(ctx, device, :unrecognized_flag)) => (true, "error")
                 for flag in [:profile, :out_of_order]
-                    Base.gc()
                     @fact @throws_pred(cl.CmdQueue(ctx, (flag, :unrecognized_flag))) => (true, "error")
                     @fact @throws_pred(cl.CmdQueue(ctx, device, (:unrecognized_flag, flag))) => (true, "error")
                     @fact @throws_pred(cl.CmdQueue(ctx, (flag, flag))) => (true, "error")
