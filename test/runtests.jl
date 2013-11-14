f7fff7686501c849a77607c733f976b2b1fbec43
Jake Bolewski <jakebolewski@gmail.com>
2013-11-13 21:55:56 -0500

    add runtests note for faster test execution

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,9 @@ testsdir = dirname(Base.source_path())
 for t in [:platform, :context, :device, :cmdqueue, :event, :buffer, :program, :kernel]
     tfile = joinpath(testsdir, "test_$t.jl")
     run(`julia $tfile`)
+    # On my crappy laptop, I constantly get "out of host memory" errors
+    # when evaling each file, use eval for faster test execution
+    #evalfile(tfile)
 end
 
 run(`julia $(joinpath(testsdir, "behavior_tests.jl"))`)
