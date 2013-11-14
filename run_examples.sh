46b276e00490282965d17cd52024a944d49b3a65
Jake Bolewski <jakebolewski@gmail.com>
2013-11-14 16:56:29 -0500

    add run_examples.sh

---
+++ run_examples.sh
@@ -0,0 +1,11 @@
+julia examples/demo.jl
+julia examples/performance.jl
+julia examples/hands_on_opencl/ex04/vadd_chain.jl
+julia examples/hands_on_opencl/ex05/vadd_abc.jl
+julia examples/hands_on_opencl/ex06/matmul.jl
+julia examples/hands_on_opencl/ex07/matmul.jl
+julia examples/hands_on_opencl/ex08/matmul.jl
+julia examples/hands_on_opencl/ex09/pi_ocl.jl
+julia examples/hands_on_opencl/exA/pi_vocl.jl 1  
+julia examples/hands_on_opencl/exA/pi_vocl.jl 4  
+julia examples/hands_on_opencl/exA/pi_vocl.jl 8 
