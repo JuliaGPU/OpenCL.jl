106e871a7fa9edf5e481144624ba6925c39a0a30
Jake Bolewski <jakebolewski@gmail.com>
2014-02-09 17:47:59 -0500

    udate clast test to reflect recent changes

--- test/compiler/clast.jl
+++ test/compiler/clast.jl
@@ -4,33 +4,26 @@ using OpenCL.CLAst
 using OpenCL.SourceGen
 using OpenCL.Compiler
 
-facts("Generation") do
-    ast = CBinOp(CNum(1), 
-                 CAdd(), 
-                 CNum(1),
-                 Int64)
+#TODO: Ensure correct number printing
+facts("OpenCL source generation") do
+    ast = CBinOp(CNum(1), CAdd(), CNum(1), Int64)
     code = clsource(ast) 
-    @fact code => "1 + 1"
+    @fact code => "(1l) + (1l)"
 
-    ast = CUnaryOp(CUAdd(), 
-                   CName("foo"), 
-                   Int64)
+    ast = CUnaryOp(CUAdd(), CName("foo"), Int64)
     code = clsource(ast) 
-    @fact code => "++(foo)"
+    @fact code => "(++(foo))"
 
     ast = CAssign(CName("foo"), 
-                  CBinOp(CNum(1),
-                         CAdd(),
-                         CNum(1),
-                         Int64),
+                  CBinOp(CNum(1), CAdd(), CNum(1), Int64),
                   Int64)
     code = clsource(ast)
-    @fact code => "foo = 1 + 1"
+    @fact code => "foo = (1l) + (1l)"
 
     ast = CBlock([CFunctionCall("foo", [], Void), 
                   CFunctionCall("bar", [], Void)])
     code = clsource(ast) 
-    @fact code => "{{\n  foo();\n  bar();\n}}\n"
+    @fact code => "{{\n\tfoo();\n\tbar();\n}}\n"
 
     ast = CFor(CAssign(CName("i"), 
                        CNum(0),
@@ -46,7 +39,7 @@ facts("Generation") do
                                      CNum(1),
                                      Int64)]))
     code = clsource(ast) 
-    @fact code => "for (i = 0; i <= 10; ++(i)) {{\n  i = 1;\n}}\n"
+    @fact code => "for (i = 0l; (i) <= (10l); (++(i))) {{\n\ti = 1l;\n}}\n"
 
     ast = CAssign(CSubscript(CName("test"),
                              CIndex(CNum(1)),
@@ -54,5 +47,7 @@ facts("Generation") do
                   CNum(10),
                   Int64)
     code = clsource(ast) 
-    @fact code => "test[1] = 10"
+    @fact code => "test[1l] = 10l"
+
+    #TODO: kernel.. 
 end
