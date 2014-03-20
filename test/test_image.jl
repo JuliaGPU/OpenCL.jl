a199288b2c23bfdca886c14ab32616e117bbbc25
Jake Bolewski <jakebolewski@gmail.com>
2014-03-19 23:05:08 -0400

    work towards 3D image support

--- test/test_image.jl
+++ test/test_image.jl
@@ -6,6 +6,8 @@ const cl = OpenCL
 
 macro throws_pred(ex) FactCheck.throws_pred(ex) end 
 
+# These tests were adapted from the PyOpenCL library
+
 facts("OpenCL.Image type constructor") do
     @fact @throws_pred(cl.Image{cl.RGBA, Float32}) => (false, "no error")
 end
@@ -30,7 +32,9 @@ const image2d_src = """
 	size_t d0 = get_global_id(0);
 	size_t d1 = get_global_id(1);
 	
-	const sampler_t samp = CLK_NORMALIZED_COORDS_FALSE | CLK_ADDRESS_CLAMP | CLK_FILTER_NEAREST;
+	const sampler_t samp = CLK_NORMALIZED_COORDS_FALSE 
+		             | CLK_ADDRESS_CLAMP
+			     | CLK_FILTER_NEAREST;
         	
 	dest[d0 * stride0 + d1] = read_imagef(src, samp, (float2)(d1, d0)).x;
 }
@@ -63,5 +67,58 @@ facts("OpenCL.Image 2D test") do
 
         a_result = reshape(cl.read(queue, a_dst), size(a))
 	@fact isapprox(norm(a_result - a), zero(Float32)) => true
+    end
+end
+
+const image3d_src = """
+ __kernel void copy_image(
+	__global float2 *dest,
+	__read_only image3d_t src,
+	int stride0,
+	int stride1)
+{
+	size_t d0 = get_global_id(0);
+	size_t d1 = get_global_id(1);
+        size_t d2 = get_global_id(2);
+
+	const sampler_t samp = CLK_NORMALIZED_COORDS_FALSE 
+		             | CLK_ADDRESS_CLAMP
+			     | CLK_FILTER_NEAREST;
+	
+	dest[d0*stride0 + d1*stride1 + d2] = read_imagef(src, samp, 
+							 (float4)(d2, d1, d0,0)).xy;
+}
+"""
+
+facts("OpenCL.Image 3D test") do
+    for device in cl.devices()
+	if !(device[:has_image_support])
+	    warn("OpenCL.Image not supported on $device")
+	    continue
+        end
+        
+	ctx   = cl.Context(device)
+	queue = cl.CmdQueue(ctx)
+
+	prg  = cl.Program(ctx, source=image3d_src) |> cl.build!
+        copy_image = cl.Kernel(prg, "copy_image")
+
+	if !(cl.Image{cl.Red, Float32} in cl.supported_image_types(ctx))
+            warn("OpenCL.Image type not supported on $device")
+	    continue
+        end
+	
+	a = rand(Float32, (10,10,10,2))
+	a_img = cl.Image{cl.RG, Float32}(ctx, (:r, :copy), hostbuf=a)
+	a_dst = cl.Buffer(Float32, ctx, :rw, length(a))
+
+	x_stride = int32(strides(a)[1])
+	y_stride = int32(strides(a)[2])
+	evt = copy_image[queue, (10,10,10)](a_dst, a_img, x_stride, y_stride)
+
+        a_result = reshape(cl.read(queue, a_dst), size(a))
+	@show a
+	@show a_result
+	@fact isapprox(sum(a_result - a), zero(Float32)) => true
     end
 end
