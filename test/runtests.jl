4f6700a59e0aa8261441b75c67e59e5850aae31c
Jake Bolewski <jakebolewski@gmail.com>
2014-07-17 00:17:41 -0400

    split out tests to see if that makes Travis any happier

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,14 @@
 module TestOpenCL
 	using FactCheck
-
-	@runtest OpenCL test_platform test_context test_device test_cmdqueue test_event test_buffer test_program test_kernel
-
+    
+    @runtest OpenCL test_platform 
+    @runtest OpenCL test_context 
+    @runtest OpenCL test_device 
+    @runtest OpenCL test_cmdqueue 
+    @runtest OpenCL test_event 
+    @runtest OpenCL test_buffer 
+    @runtest OpenCL test_program 
+    @runtest OpenCL test_kernel
 	@runtest OpenCL behavior_tests
     exitstatus()
 
