7cfb52f88d6a8f3618558e913e74048b8ddf3c35
Valentin Churavy <v.churavy@gmail.com>
2014-08-12 19:19:11 +0200

    only use one macro

--- src/api.jl
+++ src/api.jl
@@ -17,34 +17,16 @@ end
 macro ocl_func(func, ret_type, arg_types)
     local args_in = Symbol[symbol("arg$i::$T")
                            for (i, T) in enumerate(arg_types.args)]
-    quote 
-        $(esc(func))($(args_in...)) = ccall(($(string(func)), libopencl), 
-                                            $ret_type,
-                                            $arg_types,
-                                            $(args_in...))
-    end
-end
-
-macro ocl_func_1_0(func, ret_type, arg_types) 
-    quote
-        @ocl_func($func, $ret_type, $arg_types)
-    end
-end
-
-macro ocl_func_1_1(func, ret_type, arg_types)
-    quote
-        @ocl_func($func, $ret_type, $arg_types)
-    end
-end
-
-macro ocl_func_1_2(func, ret_type, arg_types)
-    quote
-        @ocl_func($func, $ret_type, $arg_types)
-    end
+    esc(quote
+        $func($(args_in...)) = ccall(($(string(func)), libopencl),
+                                      $ret_type,
+                                      $arg_types,
+                                      $(args_in...))
+    end)
 end
 
 typealias CL_callback  Ptr{Void}
-typealias CL_user_data Any 
+typealias CL_user_data Any
 
 include("api/opencl_10.jl")
 include("api/opencl_11.jl")
--- src/api/opencl_12.jl
+++ src/api/opencl_12.jl
@@ -1,54 +1,54 @@
 #=== device apis ===#
-@ocl_func_1_2(clCreateSubDevices, CL_int,
+@ocl_func(clCreateSubDevices, CL_int,
               (CL_device_id, CL_device_partition_property, CL_uint, Ptr{CL_device_id}, Ptr{CL_uint}))
 
-@ocl_func_1_2(clRetainDevice, CL_int, (CL_device_id,))
+@ocl_func(clRetainDevice, CL_int, (CL_device_id,))
 
-@ocl_func_1_2(clReleaseDevice, CL_int, (CL_device_id,))
+@ocl_func(clReleaseDevice, CL_int, (CL_device_id,))
 
 #=== memory object apis ===#
-@ocl_func_1_2(clCreateImage, CL_mem,
+@ocl_func(clCreateImage, CL_mem,
               (CL_context, CL_mem_flags, CL_image_format, CL_image_desc, Ptr{Void}, Ptr{CL_int}))
 
 #=== program object apis ===#
-@ocl_func_1_2(clCreateProgramWithBuiltInKernels, CL_program,
+@ocl_func(clCreateProgramWithBuiltInKernels, CL_program,
               (CL_context, CL_uint, Ptr{CL_device_id}, Ptr{Cchar}, Ptr{CL_int}))
 
-@ocl_func_1_2(clCompileProgram, CL_int,
-              (CL_program, CL_uint, Ptr{CL_device_id}, Ptr{CL_device_id}, Ptr{Cchar}, 
+@ocl_func(clCompileProgram, CL_int,
+              (CL_program, CL_uint, Ptr{CL_device_id}, Ptr{CL_device_id}, Ptr{Cchar},
                CL_uint, Ptr{CL_program}, Ptr{Ptr{Char}}, CL_callback, Ptr{Void}))
 
-@ocl_func_1_2(clLinkProgram, CL_program,
+@ocl_func(clLinkProgram, CL_program,
               (CL_context, CL_uint, Ptr{CL_device_id}, Ptr{Cchar}, CL_uint,
                CL_callback, Ptr{Void}, Ptr{CL_int}))
 
-@ocl_func_1_2(clUnloadPlatformCompiler, CL_int, (CL_platform_id,))
+@ocl_func(clUnloadPlatformCompiler, CL_int, (CL_platform_id,))
 
 #=== kernel object apis ===#
-@ocl_func_1_2(clGetKernelArgInfo, CL_int,
+@ocl_func(clGetKernelArgInfo, CL_int,
               (CL_kernel, CL_uint, CL_kernel_arg_info, Csize_t, Ptr{Void}, Ptr{Csize_t}))
 
 #=== enqueued commands apis ===#
-@ocl_func_1_2(clEnqueueFillBuffer, CL_int,
-              (CL_command_queue, CL_mem, Ptr{Void}, Csize_t, Csize_t, Csize_t, 
+@ocl_func(clEnqueueFillBuffer, CL_int,
+              (CL_command_queue, CL_mem, Ptr{Void}, Csize_t, Csize_t, Csize_t,
                CL_uint, Ptr{CL_event}, Ptr{CL_event}))
 
-@ocl_func_1_2(clEnqueueFillImage, CL_int,
-              (CL_command_queue, CL_mem, Ptr{Void}, Ptr{Csize_t}, Ptr{Csize_t}, 
+@ocl_func(clEnqueueFillImage, CL_int,
+              (CL_command_queue, CL_mem, Ptr{Void}, Ptr{Csize_t}, Ptr{Csize_t},
                CL_uint, Ptr{CL_event}, Ptr{CL_event}))
 
-@ocl_func_1_2(clEnqueueMigrateMemObjects, CL_int, 
+@ocl_func(clEnqueueMigrateMemObjects, CL_int,
               (CL_command_queue, CL_uint, Ptr{CL_mem}, CL_mem_migration_flags,
                CL_uint, Ptr{CL_event}, Ptr{CL_event}))
 
-@ocl_func_1_2(clEnqueueMarkerWithWaitList, CL_int,
+@ocl_func(clEnqueueMarkerWithWaitList, CL_int,
               (CL_command_queue, CL_uint, Ptr{CL_event}, Ptr{CL_event}))
 
-@ocl_func_1_2(clEnqueueBarrierWithWaitList, CL_int, 
+@ocl_func(clEnqueueBarrierWithWaitList, CL_int,
               (CL_command_queue, CL_uint, Ptr{CL_event}, Ptr{CL_event}))
 
 #=== extension function access ===#
-@ocl_func_1_2(clGetExtensionFunctionAddressForPlatform, Ptr{Void},
+@ocl_func(clGetExtensionFunctionAddressForPlatform, Ptr{Void},
               (CL_platform_id, Ptr{Cchar}))
 
 #=== deprecation ===#
