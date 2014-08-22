6e302b91d17df5aceed9f466737c03777544fc41
Jake Bolewski <jakebolewski@gmail.com>
2014-08-22 14:51:21 -0400

    formatting fixes, remove use of loadAPI macro

--- src/api.jl
+++ src/api.jl
@@ -21,41 +21,40 @@ function _ocl_func(func, ret_type, arg_types)
     quote
         function $func($(args_in...))
             ccall(($(string(func)), libopencl),
-                                            $ret_type,
-                                            $arg_types,
-                                            $(args_in...))
+                   $ret_type,
+                   $arg_types,
+                   $(args_in...))
         end
     end
 end
 
 macro ocl_func(func, ret_type, arg_types)
-    local expr = _ocl_func(func, ret_type, arg_types)
-    eval(expr)
+    _ocl_func(func, ret_type, arg_types)
 end
 
 macro deprecate_ocl_func(func, ret_type, arg_types)
-    local name = Expr(:quote,func)
+    local name = Expr(:quote, func)
     local expr = _ocl_func(func, ret_type, arg_types)
     @assert expr.args[2].head == :function
 
     local func_body = expr.args[2].args[2]
     @assert func_body.head == :block
 
-    insert!(func_body.args, 2, :(Base.depwarn(string($name," is deprecated"),
-                        $name)))
-    eval(expr)
+    insert!(func_body.args, 2, 
+            :(Base.depwarn(string($name," is deprecated"), $name)))
+    expr
 end
 
-macro loadApi(versions...)
-  for version in versions
-    include("api/opencl_$(version).jl")
-  end
+function include_api(versions...)
+    for ver in versions
+        include("api/opencl_$ver.jl")
+    end
 end
 
 typealias CL_callback  Ptr{Void}
 typealias CL_user_data Any 
 
-@loadApi "10"
+include_api(v"1.0")
 
 include("error.jl")
 include("macros.jl")
@@ -64,28 +63,27 @@ include("platform.jl")
 function parse_version(version_string)
     mg = match(r"^OpenCL ([0-9]+)\.([0-9]+) .*$", version_string)
     if mg == nothing
-        error("Non conform version string: $(ver)")
+        error("Non conforming version string: $(ver)")
     end
     return VersionNumber(int(mg.captures[1]), int(mg.captures[2]))
 end
 
-# Todo check macro
 function __init__()
-  versions = map(platforms()) do platform
-    parse_version(platform[:version])
-  end
+    versions = map(platforms()) do platform
+        parse_version(platform[:version])
+    end
 
-  global const OPENCL_VERSION = maximum(versions)
+    global const OPENCL_VERSION = maximum(versions)
 
-  if OPENCL_VERSION == v"1.1"
-    @loadApi "11"
-
-  elseif OPENCL_VERSION == v"1.2"
-    @loadApi "11" "12"
-  end
+    if OPENCL_VERSION == v"1.1"
+        include_api(v"1.1")
+    elseif OPENCL_VERSION == v"1.2"
+        include_api(v"1.1", v"1.2")
+    end
 end
 
 if VERSION < v"0.3-"
-  __init__()
+    __init__()
 end
+
 end
