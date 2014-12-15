c5114ba7db6edb1e3c089fac3a08c77ace97ed4e
Valentin Churavy <v.churavy@gmail.com>
2014-12-15 14:04:10 +0100

    update types and constants for OpenCL 2.0

--- src/types.jl
+++ src/types.jl
@@ -34,16 +34,19 @@ typealias CL_device_fp_config           CL_bitfield
 typealias CL_device_mem_cache_type      CL_uint
 typealias CL_device_local_mem_type      CL_uint
 typealias CL_device_exec_capabilities   CL_bitfield
+typealias CL_device_svm_capabilities    CL_bitfield
 typealias CL_command_queue_properties   CL_bitfield
 typealias CL_device_partition_property  Cssize_t #intptr_t
 typealias CL_device_affinity_domain     CL_bitfield
 
 typealias CL_context_properties         Cssize_t #intptr_t
 typealias CL_context_info               CL_uint
+typealias CL_queue_properties           CL_bitfield
 typealias CL_command_queue_info         CL_uint
 typealias CL_channel_order              CL_uint
 typealias CL_channel_type               CL_uint
 typealias CL_mem_flags                  CL_bitfield
+typealias CL_svm_mem_flags              CL_bitfield
 typealias CL_mem_object_type            CL_uint
 typealias CL_mem_info                   CL_uint
 typealias CL_mem_migration_flags        CL_bitfield 
@@ -53,6 +56,8 @@ typealias CL_addressing_mode            CL_uint
 typealias CL_filter_mode                CL_uint
 typealias CL_sampler_info               CL_uint
 typealias CL_map_flags                  CL_bitfield
+typealias CL_pipe_properties            Cssize_t #intptr_t
+typealias CL_pipe_info                  CL_uint
 typealias CL_program_info               CL_uint
 typealias CL_program_build_info         CL_uint
 typealias CL_build_status               CL_int
@@ -61,6 +66,8 @@ typealias CL_kernel_work_group_info     CL_uint
 typealias CL_event_info                 CL_uint
 typealias CL_command_type               CL_uint
 typealias CL_profiling_info             CL_uint
+typealias CL_sampler_properties         CL_bitfield
+typealias CL_kernel_exec_info           CL_uint
 
 # Scalar OpenGL types ! We should get these from OpenGL.jl
 
@@ -130,14 +137,17 @@ cl_device_fp_config(x)         = cl_bitfield(x)
 cl_device_mem_cache_type(x)    = cl_uint(x)
 cl_device_local_mem_type(x)    = cl_uint(x)
 cl_device_exec_capabilities(x) = cl_bitfield(x)
+cl_device_svm_capabilities(x)  = cl_bitfield(x)
 cl_command_queue_properties(x) = cl_bitfield(x)
 
 cl_context_properties(x)       = convert(CL_context_properties, x)
 cl_context_info(x)             = cl_uint(x)
+cl_queue_properties(x)         = cl_bitfield(x)
 cl_command_queue_info(x)       = cl_uint(x)
 cl_channel_order(x)            = cl_uint(x)
 cl_channel_type(x)             = cl_uint(x)
 cl_mem_flags(x)                = cl_bitfield(x)
+cl_svm_mem_flags(x)            = cl_bitfield(x)
 cl_mem_object_type(x)          = cl_uint(x)
 cl_mem_info(x)                 = cl_uint(x)
 cl_image_info(x)               = cl_uint(x)
@@ -146,6 +156,8 @@ cl_addressing_mode(x)          = cl_uint(x)
 cl_filter_mode(x)              = cl_uint(x)
 cl_sampler_info(x)             = cl_uint(x)
 cl_map_flags(x)                = cl_bitfield(x)
+cl_pipe_properties(x)          = convert(CL_pipe_properties, x)
+cl_pipe_info(x)                = cl_uint(x)
 cl_program_info(x)             = cl_uint(x)
 cl_program_build_info(x)       = cl_uint(x)
 cl_build_status(x)             = cl_int(x)
@@ -154,5 +166,7 @@ cl_kernel_work_group_info(x)   = cl_uint(x)
 cl_event_info(x)               = cl_uint(x)
 cl_command_type(x)             = cl_uint(x)
 cl_profiling_info(x)           = cl_uint(x)
+cl_sampler_properties          = cl_bitfield(x)
+cl_kernel_exec(x)              = cl_uint(x)
 
 cl_platform_id(x) = convert(Ptr{Void}, x)
