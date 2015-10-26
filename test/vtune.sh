8449499e26cc6ed5119982878401d7c53ca7ef9a
Jake Bolewski <jakebolewski@gmail.com>
2015-10-26 11:44:27 -0400

    remove vtune script

--- test/vtune.sh
+++
@@ -1,4 +0,0 @@
-/home/jake/opt/vtune_amplifier_xe_2013_update15/opt/intel/vtune_amplifier_xe_2013/bin64/amplxe-cl -run-pass-thru=--no-altstack -collect hotspots -target-duration-type=veryshort -app-working-dir /usr/bin -- /usr/bin/julia /home/jake/.julia/OpenCL/test/clcompiler.jl
-
-
-
