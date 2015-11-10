e3b855e0a9141739658022451cd5005bc5db61c0
Andrei Zhabinski <a.zhabinski@adform.com>
2015-11-11 02:03:37 +0300

    original array test (still first in list)

--- test/test_array.jl
+++ test/test_array.jl
@@ -1,28 +1,5 @@
 import OpenCL.CLArray
 
-# using Base.Test
-
-# for device in cl.devices()
-    
-#     ctx = cl.Context(device)
-#     queue = cl.CmdQueue(ctx)
-#     hostarray = zeros(Float32, 32*64)
-#     A = CLArray(ctx, hostarray)
-    
-#     @assert CLArray(ctx, queue, (:rw, :copy), hostarray) != nothing
-    
-#     @assert CLArray(ctx, hostarray,
-#                   queue=queue, flags=(:rw, :copy)) != nothing 
-    
-#     @assert CLArray(ctx, hostarray) != nothing
-    
-#     @assert CLArray(cl.Buffer(Float32, ctx, (:r, :copy), hostbuf=hostarray),
-#                     (32, 64)) != nothing
-    
-#     @assert copy(A) == A
-# end
-
-
 facts("OpenCL.CLArray") do
 
     context("OpenCL.CLArray constructors") do
@@ -40,8 +17,8 @@ facts("OpenCL.CLArray") do
 
             @fact CLArray(ctx, hostarray) --> not(nothing) "no error"
 
-            # @fact CLArray(cl.Buffer(Float32, ctx, (:r, :copy), hostbuf=hostarray),
-            #               (32, 64)) --> not(nothing) "no error"
+            @fact CLArray(cl.Buffer(Float32, ctx, (:r, :copy), hostbuf=hostarray),
+                          (32, 64)) --> not(nothing) "no error"
 
             @fact copy(A) --> A
         end
@@ -74,20 +51,14 @@ facts("OpenCL.CLArray") do
      end
 
     context("OpenCL.CLArray transpose") do
-        # Travis has too little LocalMemory, so it's not possible to test
-        # optimized transpose
-        if ENV["TRAVIS"] != "true"
-            for device in cl.devices()
-                ctx = cl.Context(device)
-                queue = cl.CmdQueue(ctx)
-                A = CLArray(ctx, rand(Float32, 32, 64))
-                B = cl.zeros(Float32, queue, 64, 32)
-                Base.transpose!(B, A)
-                @fact cl.to_host(A') --> cl.to_host(B)
-            end
-        else
-            warn("Running tests on Travis, not checking transpose")
-        end
+        for device in cl.devices()
+            ctx = cl.Context(device)
+            queue = cl.CmdQueue(ctx)
+            A = CLArray(ctx, rand(Float32, 32, 64))
+            B = cl.zeros(Float32, queue, 64, 32)
+            Base.transpose!(B, A)
+            @fact cl.to_host(A') --> cl.to_host(B)
+        end    
      end
 
 end
