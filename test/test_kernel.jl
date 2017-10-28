9cc2710c6c535a30b54a62277029541e3fe2f475
SimonDanisch <sdanisch@gmail.com>
2017-10-28 23:20:56 +0200

    don't test problematic kernel on problematic osx

--- test/test_kernel.jl
+++ test/test_kernel.jl
@@ -1,4 +1,8 @@
-
+immutable CLTestStruct
+    f1::NTuple{3, Float32}
+    f2::Void
+    f3::Float32
+end
 
 @testset "OpenCL.Kernel" begin
 
@@ -188,6 +192,38 @@
         end
     end
 
+
+    test_source = "
+    struct __attribute__((packed)) Test2{
+        long f1;
+        int __attribute__((aligned (8))) f2;
+    };
+    __kernel void structest(__global float *out, struct Test2 b){
+        out[0] = b.f1;
+        out[1] = b.f2;
+    }
+    "
+    for device in cl.devices()
+        if device[:platform][:name] == "Portable Computing Language"
+            warn("Skipping OpenCL.Kernel constructor for " *
+                 "Portable Computing Language Platform")
+            continue
+        end
+        if is_apple()
+            continue
+        end
+        ctx = cl.Context(device)
+        prg = cl.Program(ctx, source = test_source)
+        queue = cl.CmdQueue(ctx)
+        cl.build!(prg)
+        structkernel = cl.Kernel(prg, "structest")
+        out = cl.Buffer(Float32, ctx, :w, 2)
+        bstruct = (1, Int32(4))
+        structkernel[queue, (1,)](out, bstruct)
+        r = cl.read(queue, out)
+        @test r  == [1f0, 4f0]
+    end
+
     test_source = "
     //packed
     struct __attribute__((packed)) Test{
@@ -196,41 +232,29 @@
         // you might need to define the alignement of fields to match julia's layout
         float f3; // for the types used here the alignement matches though!
     };
-    // this is a case where Julia and OpenCL packed alignment would differ, so we need to specify it explicitely
-    struct __attribute__((packed)) Test2{
-        long f1;
-        int __attribute__((aligned (8))) f2; // opencl would align this to 4 in packed layout, while Julia uses 8!
-    };
-
-    __kernel void structest(__global float *out, struct Test a, struct Test2 b){
+    __kernel void structest(__global float *out, struct Test a){
         out[0] = a.f1.x;
         out[1] = a.f1.y;
         out[2] = a.f1.z;
         out[3] = a.f3;
-
-        out[4] = b.f1;
-        out[5] = b.f2;
     }
     "
 
-    @testset "OpenCL.Kernel convert structs" begin
-        for device in cl.devices()
-            if device[:platform][:name] == "Portable Computing Language"
-                warn("Skipping OpenCL.Kernel constructor for " *
-                     "Portable Computing Language Platform")
-                continue
-            end
-            ctx = cl.Context(device)
-            prg = cl.Program(ctx, source = test_source)
-            queue = cl.CmdQueue(ctx)
-            cl.build!(prg)
-            structkernel = cl.Kernel(prg, "structest")
-            out = cl.Buffer(Float32, ctx, :w, 6)
-            astruct = CLTestStruct((1f0, 2f0, 3f0), nothing, 22f0)
-            bstruct = (1, Int32(4))
-            structkernel[queue, (1,)](out, astruct, bstruct)
-            r = cl.read(queue, out)
-            @test r == [1f0, 2f0, 3f0, 22f0, 1f0, 4f0]
+    for device in cl.devices()
+        if device[:platform][:name] == "Portable Computing Language"
+            warn("Skipping OpenCL.Kernel constructor for " *
+                 "Portable Computing Language Platform")
+            continue
         end
+        ctx = cl.Context(device)
+        prg = cl.Program(ctx, source = test_source)
+        queue = cl.CmdQueue(ctx)
+        cl.build!(prg)
+        structkernel = cl.Kernel(prg, "structest")
+        out = cl.Buffer(Float32, ctx, :w, 4)
+        astruct = CLTestStruct((1f0, 2f0, 3f0), nothing, 22f0)
+        structkernel[queue, (1,)](out, astruct)
+        r = cl.read(queue, out)
+        @test r == [1f0, 2f0, 3f0, 22f0]
     end
 end
