08d3885483cee10ee68fff22896863fb00dec1a7
Tim Besard <tim.besard@gmail.com>
2024-09-10 12:00:32 +0200

    Add a native compiler, using SPIR-V IL.

--- src/OpenCL.jl
+++ src/OpenCL.jl
@@ -1,5 +1,9 @@
 module OpenCL
 
+using GPUCompiler
+using LLVM, LLVM.Interop
+using SPIRV_LLVM_Translator_unified_jll
+using Adapt
 using Reexport
 
 # library wrappers
@@ -7,8 +11,13 @@ include("../lib/CL.jl")
 @reexport using .cl
 export cl
 
+# device functionality
+include("device/runtime.jl")
+
 # high-level functionality
 include("util.jl")
 include("array.jl")
+include("compiler/compilation.jl")
+include("compiler/execution.jl")
 
 end
---
+++ src/device/runtime.jl
@@ -0,0 +1,11 @@
+signal_exception() = return
+
+malloc(sz) = C_NULL
+
+report_oom(sz) = return
+
+report_exception(ex) = return
+
+report_exception_name(ex) = return
+
+report_exception_frame(idx, func, file, line) = return
