9f2a32ec8c2a059bd87b7fa28d8bdea7081b4f71
Tim Besard <tim.besard@gmail.com>
2024-09-26 17:03:34 +0200

    Switch to a parallel test runner.

---
+++ test/setup.jl
@@ -0,0 +1,118 @@
+using Distributed, Test
+using OpenCL, pocl_jll
+using IOCapture
+
+using Random
+
+
+## entry point
+
+function runtests(f, name)
+    old_print_setting = Test.TESTSET_PRINT_ENABLE[]
+    Test.TESTSET_PRINT_ENABLE[] = false
+
+    try
+        # generate a temporary module to execute the tests in
+        mod_name = Symbol("Test", rand(1:100), "Main_", replace(name, '/' => '_'))
+        mod = @eval(Main, module $mod_name end)
+        @eval(mod, using Test, Random, OpenCL)
+
+        let id = myid()
+            wait(@spawnat 1 print_testworker_started(name, id))
+        end
+
+        ex = quote
+            GC.gc(true)
+            Random.seed!(1)
+            OpenCL.allowscalar(false)
+
+            @timed @testset $"$name" begin
+                @testset "\$(device.name)" for platform in cl.platforms(),
+                                               device in cl.devices(platform)
+
+                    cl.platform!(platform)
+                    cl.device!(device)
+
+                    $f()
+                end
+            end
+        end
+        data = Core.eval(mod, ex)
+        #data[1] is the testset
+
+        # process results
+        cpu_rss = Sys.maxrss()
+        if VERSION >= v"1.11.0-DEV.1529"
+            tc = Test.get_test_counts(data[1])
+            passes,fails,error,broken,c_passes,c_fails,c_errors,c_broken =
+                tc.passes, tc.fails, tc.errors, tc.broken, tc.cumulative_passes,
+                tc.cumulative_fails, tc.cumulative_errors, tc.cumulative_broken
+        else
+            passes,fails,errors,broken,c_passes,c_fails,c_errors,c_broken =
+                Test.get_test_counts(data[1])
+        end
+        if data[1].anynonpass == false
+            data = ((passes+c_passes,broken+c_broken),
+                    data[2],
+                    data[3],
+                    data[4],
+                    data[5])
+        end
+        res = vcat(collect(data), cpu_rss)
+
+        GC.gc(true)
+        res
+    finally
+        Test.TESTSET_PRINT_ENABLE[] = old_print_setting
+    end
+end
+
+
+## auxiliary stuff
+
+# NOTE: based on test/pkg.jl::capture_stdout, but doesn't discard exceptions
+macro grab_output(ex)
+    quote
+        mktemp() do fname, fout
+            ret = nothing
+            open(fname, "w") do fout
+                redirect_stdout(fout) do
+                    ret = $(esc(ex))
+                end
+            end
+            ret, read(fname, String)
+        end
+    end
+end
+
+# Run some code on-device
+macro on_device(ex...)
+    code = ex[end]
+    kwargs = ex[1:end-1]
+
+    @gensym kernel
+    esc(quote
+        let
+            function $kernel()
+                $code
+                return
+            end
+
+            OpenCL.@sync @OpenCL $(kwargs...) $kernel()
+        end
+    end)
+end
+
+# helper function for sinking a value to prevent the callee from getting optimized away
+@inline sink(i::Int32) =
+    Base.llvmcall("""%slot = alloca i32
+                     store volatile i32 %0, i32* %slot
+                     %value = load volatile i32, i32* %slot
+                     ret i32 %value""", Int32, Tuple{Int32}, i)
+@inline sink(i::Int64) =
+    Base.llvmcall("""%slot = alloca i64
+                     store volatile i64 %0, i64* %slot
+                     %value = load volatile i64, i64* %slot
+                     ret i64 %value""", Int64, Tuple{Int64}, i)
+
+nothing # File is loaded via a remotecall to "include". Ensure it returns "nothing".
