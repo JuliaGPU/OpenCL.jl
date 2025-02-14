202a63ec124617a3170666853896ff0c8903c63b
Tim Besard <tim.besard@gmail.com>
2025-02-14 07:15:12 +0100

    Conform more strictly to the SPIR-V/OpenCL spec.

--- lib/intrinsics/src/work_item.jl
+++ lib/intrinsics/src/work_item.jl
@@ -1,23 +1,25 @@
 # Work-Item Functions
+#
+# https://registry.khronos.org/OpenCL/specs/3.0-unified/html/OpenCL_Env.html#_built_in_variables
 
 # NOTE: these functions now unsafely truncate to Int to avoid top bit checks.
 #       we should probably use range metadata instead.
 
 # 1D values
-for (julia_name, (spirv_name, offset)) in [
+for (julia_name, (spirv_name, julia_type, offset)) in [
         # indices
-        :get_global_linear_id           => (:BuiltInGlobalLinearId, 1),
-        :get_local_linear_id            => (:BuiltInLocalInvocationIndex, 1),
-        :get_sub_group_id               => (:BuiltInSubgroupId, 1),
-        :get_sub_group_local_id         => (:BuiltInSubgroupLocalInvocationId, 1),
+        :get_global_linear_id           => (:BuiltInGlobalLinearId, Csize_t, 1),
+        :get_local_linear_id            => (:BuiltInLocalInvocationIndex, Csize_t, 1),
+        :get_sub_group_id               => (:BuiltInSubgroupId, UInt32, 1),
+        :get_sub_group_local_id         => (:BuiltInSubgroupLocalInvocationId, UInt32, 1),
         # sizes
-        :get_work_dim                   => (:BuiltInWorkDim, 0),
-        :get_sub_group_size             => (:BuiltInSubgroupSize, 0),
-        :get_max_sub_group_size         => (:BuiltInSubgroupMaxSize, 0),
-        :get_num_sub_groups             => (:BuiltInNumSubgroups, 0),
-        :get_enqueued_num_sub_groups    => (:BuiltInNumEnqueuedSubgroups, 0)]
+        :get_work_dim                   => (:BuiltInWorkDim, UInt32, 0),
+        :get_sub_group_size             => (:BuiltInSubgroupSize, UInt32, 0),
+        :get_max_sub_group_size         => (:BuiltInSubgroupMaxSize, UInt32, 0),
+        :get_num_sub_groups             => (:BuiltInNumSubgroups, UInt32, 0),
+        :get_enqueued_num_sub_groups    => (:BuiltInNumEnqueuedSubgroups, UInt32, 0)]
     gvar_name = Symbol("@__spirv_$(spirv_name)")
-    width = Int === Int64 ? 64 : 32
+    width = sizeof(julia_type) * 8
     @eval begin
         export $julia_name
         @device_function $julia_name() =
@@ -28,7 +30,7 @@ for (julia_name, (spirv_name, offset)) in [
                          ret i$(width) %val
                      }
                      attributes #0 = { alwaysinline }
-                """, "entry"), UInt, Tuple{}) % Int + $offset
+                """, "entry"), $julia_type, Tuple{}) % Int + $offset
     end
 end
 
