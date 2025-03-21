f2ff5dbf74cf1f2a3e9cf605525bc16d8a83659b
Benoît Legat <benoit.legat@gmail.com>
2025-03-21 11:34:05 +0100

    Typo fix : pivate -> private

--- examples/hands_on_opencl/ex07/matmul.jl
+++ examples/hands_on_opencl/ex07/matmul.jl
@@ -142,7 +142,7 @@ for i in 1:COUNT
 end
 
 #--------------------------------------------------------------------------------
-# OpenCL matrix multiplication ... C row per work item, A row in pivate memory
+# OpenCL matrix multiplication ... C row per work item, A row in private memory
 #--------------------------------------------------------------------------------
 kernel_source = read(joinpath(src_dir, "C_row_priv.cl"), String)
 prg  = cl.Program(source=kernel_source) |> cl.build!
