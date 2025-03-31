560228b97d61761885d5742aa579be2d73a441a8
Benoît Legat <benoit.legat@gmail.com>
2025-03-31 16:29:11 +0200

    Fix

--- test/runtests.jl
+++ test/runtests.jl
@@ -30,13 +30,13 @@ if do_help
     println("""
         Usage: runtests.jl [--help] [--list] [--jobs=N] [TESTS...]
 
-               --help                        Show this text.
-               --list                        List all available tests.
-               --verbose                     Print more information during testing.
-               --quickfail                   Fail the entire run as soon as a single test errored.
-               --jobs=N                      Launch `N` processes to perform tests (default: Sys.CPU_THREADS).
-               --platform=NAME               Run tests on the platform named `NAME` (default: all platforms).
-               --check-bounds={yes*|no|auto} Julia is launched with this argument
+                       --help                        Show this text.
+                       --list                        List all available tests.
+                       --verbose                     Print more information during testing.
+                       --quickfail                   Fail the entire run as soon as a single test errored.
+                       --jobs=N                      Launch `N` processes to perform tests (default: Sys.CPU_THREADS).
+                       --platform=NAME               Run tests on the platform named `NAME` (default: all platforms).
+                       --check-bounds={yes*|no|auto} Julia is launched with this argument
 
                Remaining arguments filter the tests that will be executed.""")
     exit(0)
@@ -105,6 +105,8 @@ if do_list
 end
 ## --platform selector
 do_platform, platform = extract_flag!(ARGS, "--platform", nothing)
+## --check-bounds
+_, check_bounds = extract_flag!(ARGS, "--check-bounds", "yes")
 ## no options should remain
 optlike_args = filter(startswith("-"), ARGS)
 if !isempty(optlike_args)
@@ -122,7 +124,6 @@ const test_exeflags = Base.julia_cmd()
 filter!(test_exeflags.exec) do c
     return !(startswith(c, "--depwarn") || startswith(c, "--check-bounds"))
 end
-_, check_bounds = extract_flag!(ARGS, "--check-bounds", "yes")
 push!(test_exeflags.exec, "--check-bounds=$check_bounds")
 push!(test_exeflags.exec, "--startup-file=no")
 push!(test_exeflags.exec, "--depwarn=yes")
