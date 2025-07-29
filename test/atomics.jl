20a1dc7db27b8552372cca3e8b7f2de24973eb92
Simeon David Schaub <simeon@schaub.rocks>
2025-07-29 17:32:10 +0200

    check for int64 atomic support

--- test/atomics.jl
+++ test/atomics.jl
@@ -6,9 +6,11 @@ function atomic_count(counter)
 end
 
 @testset "atomic_add! ($T)" for T in [Int32, UInt32, Int64, UInt64]
-    a = OpenCL.zeros(T)
-    @opencl global_size=1000 atomic_count(a)
-    @test OpenCL.@allowscalar a[] == 1000
+    if sizeof(T) == 4 || "cl_khr_int64_extended_atomics" in cl.device().extensions
+        a = OpenCL.zeros(T)
+        @opencl global_size=1000 atomic_count(a)
+        @test OpenCL.@allowscalar a[] == 1000
+    end
 end
 
 end
