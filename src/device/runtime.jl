5ec8245d01dc05d8858e3f629f48679a9fd3e314
Simeon David Schaub <simeon@schaub.rocks>
2025-09-12 09:27:30 +0200

    adapt to GPUCompiler changes

--- src/device/runtime.jl
+++ src/device/runtime.jl
@@ -20,9 +20,46 @@ struct KernelState
 end
 
 @inline @generated kernel_state() = GPUCompiler.kernel_state_value(KernelState)
-@inline @generated random_keys() = GPUCompiler.call_custom_noarg_readnone_intrinsic(
-    LLVMPtr{UInt32, AS.Workgroup}, "julia.spirv.random_keys", "random_keys",
-)
-@inline @generated random_counters() = GPUCompiler.call_custom_noarg_readnone_intrinsic(
-    LLVMPtr{UInt32, AS.Workgroup}, "julia.spirv.random_counters", "random_counters",
-)
+
+function additional_arg_intr(mod::LLVM.Module, T_state, name)
+    state_intr = if haskey(functions(mod), "julia.spirv.$name")
+        functions(mod)["julia.spirv.$name"]
+    else
+        LLVM.Function(mod, "julia.spirv.$name", LLVM.FunctionType(T_state))
+    end
+    push!(function_attributes(state_intr), EnumAttribute("readnone", 0))
+
+    return state_intr
+end
+
+# run-time equivalent
+function additional_arg_value(state, name)
+    @dispose ctx=Context() begin
+        T_state = convert(LLVMType, state)
+
+        # create function
+        llvm_f, _ = create_function(T_state)
+        mod = LLVM.parent(llvm_f)
+
+        # get intrinsic
+        state_intr = additional_arg_intr(mod, T_state, name)
+        state_intr_ft = function_type(state_intr)
+
+        # generate IR
+        @dispose builder=IRBuilder() begin
+            entry = BasicBlock(llvm_f, "entry")
+            position!(builder, entry)
+
+            val = call!(builder, state_intr_ft, state_intr, Value[], name)
+
+            ret!(builder, val)
+        end
+
+        call_function(llvm_f, state)
+    end
+end
+
+for name in [:random_keys, :random_counters]
+    @eval @inline @generated $name() =
+        additional_arg_value(LLVMPtr{UInt32, AS.Workgroup}, $(String(name)))
+end
