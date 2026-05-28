7f1fed8f813213a5c95f06419433e1a4df0d137f
Tim Besard <tim.besard@gmail.com>
2026-05-28 09:06:38 +0200

    Don't unconditionally set the sub group size.
     is currently unconditionally set to the device's
    reported subgroup size (or a heuristic default). However, the spec mentions:
    
    > Note that there is no guarantee for the value of get_sub_group_size()
    > even when this attribute is present, particularly when the work-group size
    > is not evenly divisible by the required sub-group size.
    
    Specifically, PoCL reports a subgroup count of 0 when using a work-group size
    that's smaller than the chosen subgroup size:
    
    The above is with the fix from this PR already, which only sets the
    attribute when explicitly requesting a subgroup size. Normally, PoCL
    determines an appropriate subgroup size per launch, so revert to that
    by not setting the attribute by default.
    
    This bug broke the RNG, which queries the sub group count. FWIW, this only
    surfaced on https://github.com/JuliaGPU/GPUCompiler.jl/pull/812, because
    previously the exception trap was simply removed by PoCL resulting in
    the subsequent memory access simply happening as if there was no OOB.

--- src/compiler/execution.jl
+++ src/compiler/execution.jl
@@ -4,7 +4,7 @@ export @opencl, clfunction
 ## high-level @opencl interface
 
 const MACRO_KWARGS = [:launch]
-const COMPILER_KWARGS = [:kernel, :name, :always_inline, :extensions, :backend, :validate]
+const COMPILER_KWARGS = [:kernel, :name, :always_inline, :extensions, :backend, :validate, :sub_group_size]
 const LAUNCH_KWARGS = [:global_size, :local_size, :queue]
 
 macro opencl(ex...)
